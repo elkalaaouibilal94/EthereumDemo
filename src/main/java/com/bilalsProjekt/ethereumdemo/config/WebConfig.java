ae6396a825f533b1924f95df2b152981c6a599cb
Bilal <elkalaaoui.bilal@gmail.com>
2024-02-14 02:56:40 +0100

    add CORS

---
+++ src/main/java/com/bilalsProjekt/ethereumdemo/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.bilalsProjekt.ethereumdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Change this to your React app's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
